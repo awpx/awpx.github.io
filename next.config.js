4a10628eebafa34d1869b3e7cfe5b8f091178381
awpx <awpxwork@gmail.com>
2022-01-23 09:03:11 -0300

    commit before fixing image export error

--- next.config.js
+++ next.config.js
@@ -85,6 +85,17 @@ module.exports = withBundleAnalyzer({
     return config
   },
   images: {
-    domains: ['live.staticflickr.com'],
-},
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    nextImageExportOptimizer: {
+      imageFolderPath: "public/static/images",
+      exportFolderPath: "out",
+      quality: 75,
+    },
+  },
+  env: {
+    storePicturesInWEBP: true,
+    generateAndUseBlurImages: true,
+  },
 })
